56b98719a80171d396435cda1a8f20519da8a391
Ryan Charles <ryan.charles@ironlight-JKQ447JRG4.localdomain>
2025-11-25 09:25:58 -0600

    fix colors again

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,22 @@
 return {
   {
     "projekt0n/github-nvim-theme",
-    lazy = false,
-    priority = 1000,
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+
+      -- vim.cmd('colorscheme github_dark')
+    end,
     opts = {
       -- choose the variant
       variant = "light_high_contrast",
     },
   },
-  { "earthbucks/LazyVim", opts = {
+  { "LazyVim/LazyVim", opts = {
     colorscheme = "github_light_high_contrast",
   } },
 }
