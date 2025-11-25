f7a882a6969cfafc25ae8ee12258e2f500acd655
Ryan Charles <ryan.charles@ironlight-JKQ447JRG4.localdomain>
2025-11-25 09:13:35 -0600

    update colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,14 @@
 return {
-  "earthbucks/LazyVim",
-  opts = {
-    -- colorscheme = "catppuccin",
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- choose the variant
+      variant = "light_high_contrast",
+    },
   },
+  { "earthbucks/LazyVim", opts = {
+    colorscheme = "github_light_high_contrast",
+  } },
 }
