5000fdd8a4a8cac41686be67ba8d23a4f0090b89
Ryan X. Charles <ryan.charles@ironlight.io>
2026-02-03 12:56:38 -0600

    disable spellcheck

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,6 +6,7 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 -- what follows are not autocmds, but we need them loading after the verylazy event
 
