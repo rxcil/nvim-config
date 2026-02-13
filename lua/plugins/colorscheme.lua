5ae28030c1c7cb71b0e9435a3bd6b83a2287114e
Ryan X. Charles <ryan.charles@ironlight.io>
2026-02-13 10:07:59 -0600

    Revert "light theme (gruvbox light)"
    This reverts commit 7dbbea7e5d850e7ba65105802a2d7131e5059f51.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,35 @@
 return {
-  {
-    "RRethy/base16-nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "base16-gruvbox-light-hard",
-    },
-  },
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   name = "github-theme",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     require("github-theme").setup({
+  --       -- ...
+  --     })
+  --
+  --     -- vim.cmd('colorscheme github_dark')
+  --   end,
+  --   opts = {
+  --     -- choose the variant
+  --     variant = "light_high_contrast",
+  --   },
+  -- },
+  -- { "LazyVim/LazyVim", opts = {
+  --   colorscheme = "github_light_high_contrast",
+  -- } },
+
+  -- Configure TokyoNight with a black background for better contrast
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     style = "night", -- darkest variant
+  --     on_colors = function(colors)
+  --       colors.bg = "#000000" -- pure black background
+  --       colors.bg_dark = "#000000" -- sidebar/float backgrounds
+  --       colors.bg_float = "#000000" -- floating window backgrounds
+  --     end,
+  --   },
+  -- },
 }
